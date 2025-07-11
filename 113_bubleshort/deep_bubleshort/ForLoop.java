a07b1073cd541dd8492df446f999ac8914208575
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-07-11 23:39:10 +0700

    Syntaxis For Loop

---
+++ 113_bubleshort/deep_bubleshort/ForLoop.java
@@ -0,0 +1,28 @@
+package deep_bubleshort;
+
+public class ForLoop {
+    
+    public static void main(String[] args) {
+        /*
+         * Syntaxis for loop
+         * for (initialization, condition, increment) {
+         *  // blok kode yang akan di eksekusi
+         * }
+         * 
+         * Hal yang bisa dilakukan didalam initialization
+         * 1. Mendeklarasikan variabel baru
+         *      for (int i=0; i<5; i++) {...}
+         * 2. Menginisialisasi variabel yang sudah ada
+         *      int i;
+         *      for (i=0; i<5; i++) {...}
+         * 3. Menginisialisasi lebih dari satu variabel (pakai koma)
+         *      for (int i=0, j=5; i<j; i++, j--) {...}
+         * 4. Mengosongkan bagian initialization (jika sudah di initialization diluar)
+         *      int i=0;
+         *      for (; i<10; i++) {...}
+         * 5. Memanggil function selama hasilnya tetap valid sebagai ekspresi
+         *      for (System.out.print("Start: " ), int i=0; i<10; i++) {...}
+         */
+    }
+
+}
