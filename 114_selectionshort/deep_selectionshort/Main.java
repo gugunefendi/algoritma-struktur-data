fca9421654245bb44d1bb75d9fb5c97d8b923b56
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-07-21 22:41:37 +0700

    Deep Selection short

--- 114_selectionshort/deep_selectionshort/Main.java
+++ 114_selectionshort/deep_selectionshort/Main.java
@@ -2,25 +2,23 @@ package deep_selectionshort;
 
 public class Main {
     public static void main(String[] args) {
-        int[] my_array = {64, 34, 25, 12, 22, 11, 90, 5};
-        int n = my_array.length;
+        int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int n = array.length;
 
-        for (int i = 0; i < n; i++) {
-            int min_index = i;
-            for (int j = i+1; j < n; j++) {
-                if (my_array[j] < my_array[min_index]) {
-                    min_index = j;
+        for (int i=0; i<n; i++) {
+            int minIndex = i;
+            for (int j=i+1; j<n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
                 }
             }
-            int temp = my_array[i];
-            my_array[i] = my_array[min_index];
-            my_array[min_index] = temp;
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
         }
 
-        System.out.print("Sorted array: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(my_array[i] + " ");
+        for (int i=0; i<n; i++) {
+            System.out.println(array[i]);
         }
-        System.out.println();
     }
 }
